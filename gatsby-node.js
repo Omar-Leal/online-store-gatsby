4e91c5f4dbbf8854760d19fdab2209d104b04a5c
Omar Leal - UbuntuLinux <omarenriquelealr@gmail.com>
2021-03-08 09:12:02 -0500

    createPages on gatsby

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,41 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+	const { createPage } = actions
+	const productTemplate = path.resolve(`src/templates/Product.js`)
+	const  result = await graphql(`
+	   query GET_DATA {			
+
+						allStripePrice {
+								edges {
+										node {
+												id 
+												unit_amount
+												product {
+															name
+															metadata {
+																		img
+																		wear
+																		description_
+															}
+												}
+										}
+								}
+						}
+
+
+		 }
+	
+	`)
+	if(result.errors) {
+		throw results.errors
+	}
+
+	result.data.allStripePrice.edges.forEach(({node}) => {
+		createPage({ 
+			path: `${node.id}`,
+      component: productTemplate,
+		 })
+	})
+}
+
---
+++ src/templates/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Products = () => {
+	return ( 
+		<div>
+			<h2>World</h2>
+		</div>
+	)
+}
+
+export default Products;
